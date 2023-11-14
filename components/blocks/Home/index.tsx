762e5f5681530792cfc980cb58f979f4f4651c42
Rostislav Martinyuk <kievstarpro01082017@gmail.com>
2023-11-14 17:10:09 +0200

    build project, removed cloudinary

--- components/blocks/Home/index.tsx
+++ components/blocks/Home/index.tsx
@@ -1,3 +1,2 @@
 export { Preview } from "./Preview/Preview";
-export { Info } from "./Info/Info";
 export { LocalBusiness } from "./LocalBusiness/LocalBusiness";
--- tina/config.ts
+++ tina/config.ts
@@ -10,15 +10,15 @@ const config = defineConfig({
   token: process.env.TINA_TOKEN!,
   media: {
     // If you wanted cloudinary do this
-    loadCustomStore: async () => {
-      const pack = await import("next-tinacms-cloudinary");
-      return pack.TinaCloudCloudinaryMediaStore;
-    },
-    // this is the config for the tina cloud media store
-    // tina: {
-    // 	publicFolder: "public",
-    // 	mediaRoot: "uploads",
+    // loadCustomStore: async () => {
+    //   const pack = await import("next-tinacms-cloudinary");
+    //   return pack.TinaCloudCloudinaryMediaStore;
     // },
+    // this is the config for the tina cloud media store
+    tina: {
+      publicFolder: "public",
+      mediaRoot: "uploads",
+    },
   },
   build: {
     publicFolder: "public",
